f81f66b6d627d903856a829422970cc806d9c8b6
Chirag Chandrashekhar <cchirag85@gmail.com>
2024-08-08 19:04:24 +0530

    Added fiber as dependency and wrote a hello world query

--- main.go
+++ main.go
@@ -2,15 +2,33 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"nexorade/dotty-go/db"
 	"os"
+
+	"github.com/gofiber/fiber/v2"
 )
 
-func main (){
-	// var connString string = fmt.Sprintf("postgres://%s:%s@%s:5432/%s", os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_HOST"), os.Getenv("POSTGRES_DB"))
-	// err := db.Init(connString)
+var port string = ":8080"
 
-	// if err != nil{
-	// 	log.Fatal(err)
-	// }
-	fmt.Printf("Hello %s", os.Getenv("POSTGRES_DB"))
+func main (){
+	var connString string = fmt.Sprintf("postgres://%s:%s@%s:5432/%s", os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_HOST"), os.Getenv("POSTGRES_DB"))
+	err := db.Init(connString)
+
+	if err != nil{
+		log.Fatal(err)
+	}
+
+
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello World")
+	})
+
+	serverErr := app.Listen(port)
+
+	if serverErr != nil{
+		log.Fatal(serverErr)
+	}
 }
